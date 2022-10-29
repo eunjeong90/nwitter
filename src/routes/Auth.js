3214121026a9b58963f5d76e8fd721668a3789ff
장은정 <jang-eunjeong@jang-eunjeong-ui-MacBookPro.local>
2022-10-29 15:35:33 +0900

    auth page onChnage 추가

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Auth() {
-  return <div>auth</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else {
+      setPassword(value);
+    }
+  };
+  console.log(email);
+  console.log(password);
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div>
+          <button>Google 계정으로 로그인하기</button>
+          <button>Facebook 계정으로 로그인하기</button>
+          <span>또는</span>
+        </div>
+        <input
+          name='email'
+          type='text'
+          placeholder='Email'
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name='password'
+          type='password'
+          placeholder='Password'
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type='submit' value='로그인하기' />
+        <div>
+          <input type='submit' value='비밀번호를 잊으셨나요?' />
+          <strong>
+            계정이 없으신가요? <Link to=''>가입하기</Link>
+          </strong>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Auth;
