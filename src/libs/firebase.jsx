7a5e8a7e9ae68bd3a2ee2f99e5b0eb4fd23d0ed0
장은정 <jang-eunjeong@jang-eunjeong-ui-MacBookPro.local>
2022-10-31 21:48:30 +0900

    jsx 확장자로 변경

---
+++ src/libs/firebase.jsx
@@ -0,0 +1,26 @@
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const authService = getAuth(firebaseApp);
+const auth = getAuth();
+// const providerData = createUserWithEmailAndPassword(auth, email, password);
+
+export { authService };
