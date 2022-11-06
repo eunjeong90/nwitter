10355cea7a0c5803f9d1938026bae4f6f8ca3e7a
장은정 <jang-eunjeong@jang-eunjeong-ui-MacBookPro.local>
2022-11-06 11:04:28 +0900

    stric mode 랜더링 두번되는 문제로 데이터베이스 두번씩 불러오는 것 방지하기 위해 임시 주석 추러

--- src/index.jsx
+++ src/index.jsx
@@ -5,8 +5,10 @@ import GlobalStyles from "styles/GlobalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <>
     <GlobalStyles />
     <App />
-  </React.StrictMode>
+  </>
+  // </React.StrictMode>
 );
