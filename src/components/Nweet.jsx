94bd75d72403dad94e38a5815ea6e6bc9cf31505
JangEunJeong <cuttlefish77777@gmail.com>
2022-11-10 22:06:55 +0900

    수정하기 기능 추가

--- src/components/Nweet.jsx
+++ src/components/Nweet.jsx
@@ -1,9 +1,11 @@
-import React from "react";
-import { deleteDoc, doc } from "firebase/firestore";
+import React, { useState } from "react";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
 import { dbService } from "libs/firebase";
 
 function Nweet({ nweetObj, isCurrentUser }) {
   const nweetRef = doc(dbService, "tweets", nweetObj.id);
+  const [updateNweet, setUpdateNweet] = useState(nweetObj.text);
+  const [updateEditor, setUpdateEditor] = useState(false);
   const handleRemoveNweet = () => {
     const confirm = window.confirm("정말 이 nweet을 삭제하시겠어요?");
     if (!confirm) {
@@ -12,18 +14,36 @@ function Nweet({ nweetObj, isCurrentUser }) {
       deleteDoc(nweetRef);
     }
   };
+  const toggleUpdateState = () => setUpdateEditor((prev) => !prev);
+  const onChangeNewNweet = ({ target: { value } }) => {
+    setUpdateNweet(value);
+  };
+  const handleUpdateNweet = (event) => {
+    event.preventDefault();
+    updateDoc(nweetRef, { text: updateNweet });
+    setUpdateEditor(false);
+  };
   return (
-    <div>
-      <p>{nweetObj.text}</p>
-      <p>{nweetObj.userName}</p>
-      <p>{nweetObj.createdAt}</p>
-      {isCurrentUser && (
-        <>
-          <button onClick={handleRemoveNweet}>삭제하기</button>
-          <button>수정하기</button>
-        </>
+    <>
+      {updateEditor && (
+        <form onSubmit={handleUpdateNweet}>
+          <input type='text' value={updateNweet} onChange={onChangeNewNweet} />
+          <input type='submit' value='수정하기' onClick={handleUpdateNweet} />
+          <input type='button' value='취소하기' onClick={toggleUpdateState} />
+        </form>
       )}
-    </div>
+      <div>
+        <p>{nweetObj.text}</p>
+        <p>{nweetObj.userName}</p>
+        <p>{nweetObj.createdAt}</p>
+        {isCurrentUser && (
+          <>
+            <button onClick={handleRemoveNweet}>삭제하기</button>
+            <button onClick={toggleUpdateState}>수정하기</button>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
