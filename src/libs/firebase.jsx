5ef2b094f27e0044ebb1afa3e10fb4b06a2d01f3
JangEunJeong <cuttlefish77777@gmail.com>
2022-11-14 21:40:36 +0900

    firebase storage 설정 진행중

--- src/libs/firebase.jsx
+++ src/libs/firebase.jsx
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -18,5 +19,6 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 const authService = getAuth(firebaseApp);
 const dbService = getFirestore(firebaseApp);
+const storageService = getStorage();
 
-export { authService, dbService };
+export { authService, dbService, storageService };
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { dbService } from "libs/firebase";
+import React, { useEffect, useRef, useState } from "react";
+import { dbService, storageService } from "libs/firebase";
 import {
   addDoc,
   getDocs,
@@ -9,12 +9,15 @@ import {
   query,
   orderBy,
 } from "firebase/firestore";
+import { ref, uploadString } from "firebase/storage";
+import { v4 as uuidv4 } from "uuid";
 import Nweet from "components/Nweet";
 
 function Home({ useObj }) {
+  const { displayName, uid } = useObj;
   const [nweet, setNweet] = useState("");
   const [nweetResult, setNweetResult] = useState([]);
-  const { displayName, uid } = useObj;
+  const [imgFile, setImgFile] = useState("");
 
   useEffect(() => {
     const q = query(
@@ -32,21 +35,39 @@ function Home({ useObj }) {
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    try {
-      const nweetPost = await addDoc(collection(dbService, "tweets"), {
-        text: nweet,
-        createdAt: new Date().toLocaleString(),
-        userName: uid,
-      });
-      setNweet("");
-      console.log("Document written with ID: ", nweetPost.id);
-      console.log(`onsubmit ${nweet}`);
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
+    // try {
+    //   const nweetPost = await addDoc(collection(dbService, "tweets"), {
+    //     text: nweet,
+    //     createdAt: new Date().toLocaleString(),
+    //     userName: uid,
+    //   });
+    //   setNweet("");
+    //   console.log("Document written with ID: ", nweetPost.id);
+    //   console.log(`onsubmit ${nweet}`);π
+    // } catch (e) {
+    //   console.error("Error adding document: ", e);
+    // }
+    const fileRef = ref(storageService, `${uid}/${uuidv4()}`);
+    const res = await uploadString(fileRef, imgFile, "data_url");
+    console.log(res);
   };
   const onChange = ({ target: { value } }) => {
     setNweet(value);
+  };
+
+  const fileInput = useRef(null);
+  const onFileChange = ({ target: { files } }) => {
+    const file = files[0];
+    const reader = new FileReader();
+    reader.onloadend = ({ currentTarget: { result } }) => {
+      console.log(result);
+      setImgFile(result);
+    };
+    reader.readAsDataURL(file);
+  };
+  const onClearFile = () => {
+    fileInput.current.value = null;
+    setImgFile(null);
   };
 
   return (
@@ -61,6 +82,19 @@ function Home({ useObj }) {
           placeholder='무슨 일이 일어나고 있나요?'
           maxLength={120}
         />
+        {imgFile && (
+          <div>
+            <img
+              src={imgFile}
+              ref={fileInput}
+              alt='첨부파일'
+              width='100px'
+              height='100px'
+            />
+            <button onClick={onClearFile}>초기화</button>
+          </div>
+        )}
+        <input type='file' accept='image/*' onChange={onFileChange} />
         <input type='submit' value='트윗하기' />
       </form>
       {nweetResult.map((post) => (
