af52abef269ee0589877dffd12daf7e1d69952c1
JangEunJeong <cuttlefish77777@gmail.com>
2022-12-03 15:38:40 +0900

    빌드시 빈페이지 오류 수정 및 파이어베이스 호스팅 파일 삭제

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -7,7 +7,7 @@ import Layout from "./Layout";
 
 function AppRouter({ isLoggedIn, useObj, refreshUser }) {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         {isLoggedIn ? (
           <>
--- src/libs/firebase.jsx
+++ src/libs/firebase.jsx
@@ -6,13 +6,13 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: process.env.REACT_APP_API_KEY || "mock_key",
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN || "mock_key",
+  databaseURL: process.env.REACT_APP_DATABASE_URL || "mock_key",
+  projectId: process.env.REACT_APP_PROJECT_ID || "mock_key",
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET || "mock_key",
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID || "mock_key",
+  appId: process.env.REACT_APP_APP_ID || "mock_key",
 };
 
 // Initialize Firebase
